030d01327f5a6ad156dcc10cb60ef7335327cb40
Law Montenegro <lauramarce501@gmail.com>
2024-02-13 09:52:13 -0500

    Agregado cambios desde la rama feature 1

--- Generadores.py
+++ Generadores.py
@@ -1,6 +1,5 @@
 '''
-Generadores 'yield'
-
+Generadores Logica
 '''
 
 
@@ -35,63 +34,18 @@ def mi_segundo_generador():
     x+=1
     yield x
     
-    
-
-def mi_generador():
-    num = 1
-    while True:
-        yield num
-        num += 1
-        
-'''
-generador = mi_generador()
-print(next(generador))
-print(next(generador))  
-'''  
 
 
 
-def mi_generador():
+def mi_tercer_generador():
     num = 7
     count = 1
     
     while True:
         yield num * count
         count +=1
-        
-        
-        
-'''
-generador = mi_generador()
-print(next(generador))
-print(next(generador))
-print(next(generador))
-print(next(generador))
-print(next(generador))
-print(next(generador))
-print(next(generador))
-print(next(generador))
 
-        '''
-        
-def mi_generado():
-    vidas =  3
-    while vidas != 0:
-        yield f"Te quedan {vidas} vidas"
-        vidas -=1
-    if vidas == 1 :
-        yield "Te queda 1 vida"
-    if vidas == 0:
-        yield "Game Over"
 
-    
-    
-perder_vida = mi_generado()  
-
-print(next(perder_vida))
-print(next(perder_vida)) 
-print(next(perder_vida)) 
-print(next(perder_vida))       
 
 
 def mensaje():
